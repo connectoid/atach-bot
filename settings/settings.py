d2ae14cfb7f9777410ff1b9d7a9a3a9ecd3aef89
Alexander Beley <beleysasha@gmail.com>
2024-05-20 16:21:40 +1200

    change data

--- settings/settings.py
+++ settings/settings.py
@@ -1,10 +1,5 @@
 data_list = [
     {
-        'date': '2024-05-30T00:00:00',
-        'departure': '{"IATA":"DYR","isCity":false}',
-        'arrival': '{"IATA":"MOW","isCity":true}',
-     },
-    {
         'date': '2024-05-24T00:00:00',
         'departure': '{"IATA":"DYR","isCity":false}',
         'arrival': '{"IATA":"MOW","isCity":true}',
